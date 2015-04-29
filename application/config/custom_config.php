9742d01249defe21bb26c7bb6524ec193ac8ed7a
Douglas Doner <ddoner01@seattlecentral.edu>
2015-04-29 13:41:25 -0700

    Fixes custom_config file to correctly load header file

--- application/config/custom_config.php
+++ application/config/custom_config.php
@@ -1,4 +1,10 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 //custom_config.php - a place to save global settings
 
+$config['style'] = 'amelia.css';
+$config['banner'] = 'Default Banner';
+$config['title'] = 'Default Title';
+$config['copyright'] = 'Default Copyright';
+$config['masthead'] = 'Default MastHead';
+$config['theme'] = 'themes/bootswatch/';
 $config['banner-href'] = './';
